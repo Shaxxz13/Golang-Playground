e829aeac231d6cfaeb711def51cf21deb05a477e
Shubhankar Kalra <44618338+shubhankarkalra@users.noreply.github.com>
2024-03-18 19:36:03 +0530

    Added URL Parse check

--- Basic/main.go
+++ Basic/main.go
@@ -2,22 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
 	"runtime"
 )
+
+// Checking HTTP Requests using input arguments
 
 func main() {
 
 	// Print OS Info
-	os := runtime.GOOS
-	fmt.Printf("Runtime OS: %v\n", os)
+	osInfo := runtime.GOOS
+	fmt.Printf("Runtime OS: %v\n", osInfo)
 
 	args := os.Args
 
 	if len(args) < 2 {
 
-		fmt.Printf("Usage: ./main <argument> <argument>\n")
-		os.Exit(10)
+		fmt.Printf("Usage: ./main <url>\n")
+		os.Exit(1)
 	}
 
-	fmt.Printf("Total Arguments Entered: %v\n1st-Nth Entry: %v\n", args, args[1:])
+	// myurl, err := url.ParseRequestURI(args[1])   :::: Could be like this if we want the URL value returned
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+
+		fmt.Printf("The URL is invalid: %s\n", err)
+
+	}
+	response, err = http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
+	defer response.Body.close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+
 }
